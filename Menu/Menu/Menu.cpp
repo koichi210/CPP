a4fea2a7db11335f0f6a4584bcfe5fc3b80c53ed
koichi <koichi210@gmail.com>
2019-01-13 10:57:40 +0900

    Menuのサンプルコード

---
+++ Menu/Menu/Menu.cpp
@@ -0,0 +1,260 @@
+// Menu.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "Menu.h"
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;								// 現在のインターフェイス
+TCHAR szTitle[MAX_LOADSTRING];					// タイトル バーのテキスト
+TCHAR szWindowClass[MAX_LOADSTRING];			// メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: ここにコードを挿入してください。
+//	GetMenu(m_hWnd);
+
+	// グローバル文字列を初期化しています。
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_MENU, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// アプリケーションの初期化を実行します:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MENU));
+
+	// メイン メッセージ ループ:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+//  コメント:
+//
+//    この関数および使い方は、'RegisterClassEx' 関数が追加された
+//    Windows 95 より前の Win32 システムと互換させる場合にのみ必要です。
+//    アプリケーションが、関連付けられた
+//    正しい形式の小さいアイコンを取得できるようにするには、
+//    この関数を呼び出してください。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MENU));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MENU);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します。
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // グローバル変数にインスタンス処理を格納します。
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:  メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND	- アプリケーション メニューの処理
+//  WM_PAINT	- メイン ウィンドウの描画
+//  WM_DESTROY	- 中止メッセージを表示して戻る
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 選択されたメニューの解析:
+		switch (wmId)
+		{
+		case IDM_ADD_MENU :
+			CreateMyMenu(hWnd);
+			break;
+
+		case IDM_DEL_MENU :
+			DeleteMyMenu(hWnd);
+			break;
+
+		case IDM_ADD_SUB_MENU :
+			CreateMySubMenu(hWnd);
+			break;
+
+		case IDM_DEL_SUB_MENU :
+			DeleteMySubMenu(hWnd);
+			break;
+
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 描画コードをここに追加してください...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+VOID CreateMyMenu(HWND hWnd)
+{
+	// メニューハンドラ取得
+	HMENU hMenu = GetMenu(hWnd);
+
+	// トップメニューの取得
+	HMENU hTestArea = GetSubMenu( hMenu, 2 );
+
+	// メニュー追加
+	AppendMenu(hTestArea, MF_BYPOSITION ,1 ,_T("Create Menu!!"));
+}
+
+VOID DeleteMyMenu(HWND hWnd)
+{
+	// メニューハンドラ取得
+	HMENU hMenu = GetMenu(hWnd);
+
+	// トップメニューの取得
+	HMENU hTestArea = GetSubMenu( hMenu, 2 );
+
+	// メニュー削除
+	DeleteMenu(hTestArea, 0, MF_BYPOSITION);
+}
+
+VOID CreateMySubMenu(HWND hWnd)
+{
+	// メニューハンドラ取得
+	HMENU hMenu = GetMenu(hWnd);
+
+	// トップメニューの取得
+	HMENU hTestArea = GetSubMenu( hMenu, 2 );
+
+	// サブメニューの取得
+	HMENU hSubMenu = GetSubMenu( hTestArea, 1 );
+
+	// メニュー追加
+	AppendMenu(hSubMenu, MF_BYPOSITION ,1 ,_T("Create SubMenu!!"));
+}
+
+VOID DeleteMySubMenu(HWND hWnd)
+{
+	// メニューハンドラ取得
+	HMENU hMenu = GetMenu(hWnd);
+
+	// トップメニューの取得
+	HMENU hTestArea = GetSubMenu( hMenu, 2 );
+
+	// サブメニューの取得
+	HMENU hSubMenu = GetSubMenu( hTestArea, 1 );
+
+	// メニュー削除
+	DeleteMenu(hSubMenu, 0, MF_BYPOSITION);
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ Menu/Menu/Menu.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "resource.h"
+
+VOID CreateMyMenu(HWND hWnd);
+VOID DeleteMyMenu(HWND hWnd);
+
+VOID CreateMySubMenu(HWND hWnd);
+VOID DeleteMySubMenu(HWND hWnd);
