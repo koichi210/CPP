1e04a24be8fcfa5fe3347256255866fbd80bd53d
koichi <koichi210@gmail.com>
2020-04-28 17:00:33 +0900

    テンプレートを使ったクラス宣言

--- TemplateConsole/TemplateConsole/TemplateConsole.cpp
+++ TemplateConsole/TemplateConsole/TemplateConsole.cpp
@@ -9,15 +9,37 @@ using namespace std;
  
 //  テンプレート関数
 template <typename T>
-T add(T x, T y){
+T FuncAdd(T x, T y)
+{
     return x + y;
 }
 
+template<typename T> class CCalc
+{
+public:
+    T m_n1;
+    T m_n2;
+
+    T add() const{
+        return m_n1 + m_n2;
+    }
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-    cout << add<string>("ABC", "def") << endl;	// stringを明示的に指定
-    cout << add<int>(12, 34) << endl;			// intを明示的に指定
-    cout << add(5, 6) << endl;					// intの場合、指定省略可能
+    cout << FuncAdd<string>("ABC", "def") << endl;	// stringを明示的に指定
+    cout << FuncAdd<int>(12, 34) << endl;			// intを明示的に指定
+    cout << FuncAdd(5, 6) << endl;					// intの場合、指定省略可能
+
+    CCalc<int> calc1;
+	calc1.m_n1 = 7;
+	calc1.m_n2 = 8;
+	cout << calc1.add() << endl;
+
+    CCalc<string> calc2;
+	calc2.m_n1 = "GHI";
+	calc2.m_n2 = "jkl";
+	cout << calc2.add() << endl;
 
 	return 0;
 }
