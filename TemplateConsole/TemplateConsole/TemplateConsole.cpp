f707432a6348bd7636fa4fbc40771e54c66226d4
koichi <koichi210@gmail.com>
2020-04-28 20:39:08 +0900

    テンプレート関数の拡張

--- TemplateConsole/TemplateConsole/TemplateConsole.cpp
+++ TemplateConsole/TemplateConsole/TemplateConsole.cpp
@@ -14,16 +14,26 @@ T FuncAdd(T x, T y)
     return x + y;
 }
 
+//  テンプレート関数（複数引数）
+template<typename T, typename S>
+S FuncMul(T x, S y)
+{
+    return x * y;
+}
+
+//  テンプレートクラス
 template<typename T> class CCalc
 {
 public:
     T m_n1;
     T m_n2;
 
-    T add() const{
+    T add() const
+	{
         return m_n1 + m_n2;
     }
 };
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -31,7 +41,9 @@ int _tmain(int argc, _TCHAR* argv[])
     cout << FuncAdd<int>(12, 34) << endl;			// intを明示的に指定
     cout << FuncAdd(5, 6) << endl;					// intの場合、指定省略可能
 
-    CCalc<int> calc1;
+    cout << FuncMul<int, double>(20, 1.5) << endl;	// 複数引数を指定
+
+	CCalc<int> calc1;
 	calc1.m_n1 = 7;
 	calc1.m_n2 = 8;
 	cout << calc1.add() << endl;
