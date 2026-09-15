5fda5191ba3409eb90b37b667fb235e04de77f44
koichi210 <koichi210@gmail.com>
2026-09-15 23:14:07 +0900

    JsonIFをお試しコミット

---
+++ JsonIF/JsonIF/JsonIFDlg.h
@@ -0,0 +1,40 @@
+﻿
+// JsonIFDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+#include "External/picojson.h"
+#include "External/rapidjson.h"
+#include "External/json.hpp"
+
+// CJsonIFDlg ダイアログ
+class CJsonIFDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CJsonIFDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_JSONIF_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedPicojson();
+	afx_msg void OnBnClickedRapidjson();
+	afx_msg void OnBnClickedNlomannjson();
+};
---
+++ JsonIF/JsonIF/resource.h
@@ -0,0 +1,20 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// JsonIF.rc で使用
+//
+#define IDD_JSONIF_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IBT_PICOJSON                    1000
+#define IBT_RAPIDJSON                   1002
+#define IBT_NLOMANNJSON                 1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
